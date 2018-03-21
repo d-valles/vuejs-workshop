15b4d635e37588ba71bc7aaf4f1032ba4926009f
David Valles <david.valles@wizeline.com>
2018-03-20 20:14:26 -0600

    Component to add task

---
+++ src/components/todo/TodoForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form v-on:submit.prevent="addTask">
+        <div class="input-group">
+          <input v-model="newTask" type="text" class="form-control">
+          <div class="input-group-btn">
+            <button class="btn btn-warning">
+              add task
+            </button>
+          </div>
+        </div>
+    </form>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            newTask: ''
+        }
+    },
+
+    methods: {
+        addTask() {
+            this.$emit('addTask', this.newTask)
+            this.newTask = ''
+        }
+    }
+}
+</script>
