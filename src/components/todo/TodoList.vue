d42f9f839a3d70cb05b339edfb19565a37d12ef3
David Valles <david.valles@wizeline.com>
2018-03-20 20:14:02 -0600

    Component for List tasks

---
+++ src/components/todo/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="list-group">
+        <li v-for="(item, index) in tasks"
+            :key="index"
+            class="list-group-item">
+          {{item.description}} - {{item.isDone ? 'done' : 'pending'}}
+          <button v-on:click="toggle(item)" class="btn btn-sm btn-primary float-right">
+            toggle
+          </button>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: {
+            type: Array,
+            default: []
+        }
+    },
+
+    methods: {
+        toggle(item) {
+            this.$emit('toggle', item)
+        }
+    }
+}
+</script>
